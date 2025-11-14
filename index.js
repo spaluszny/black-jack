8db127b3b0e012933e6a5ea8a0c201fdb56505a7
Sarah Paluszny <sarah.paluszny@gmail.com>
2025-11-14 18:09:27 -0500

    fix bug

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 
-let player = {
-    name: 'Per',
-    chips: 145
-}
+// let player = {
+//     name: 'Per',
+//     chips: 145
+// }
 let cards = []
+let dealerCards = []
 let sum = 0
+let dealerSum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let dealerSumEl = document.getElementById("dealer-sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEL = document.getElementById("player-el")
 let cardReal = document.getElementById("real-cards")
-let dealerCards = document.getElementById("dealer-cards")
+let dealerCardsEl = document.getElementById("dealer-cards")
 let stay = false
 //let dealerEl = 
 
@@ -45,8 +48,12 @@ function startGame() {
 
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
+    dealerSum = 0
+    dealerSumEl.textContent = "Dealer: " + dealerSum
+    dealerCardsEl.innerHTML = "<img src='./playing-cards/front.jpg' />"
+    stay = false
 
-    cardReal.innerHTML = "<img src='/playing-cards/" + getRandomSuite() + "-" + firstCard + ".jpg'> <img src='/playing-cards/" + getRandomSuite() + "-" + secondCard + ".jpg'>"
+    cardReal.innerHTML = "<img src='./playing-cards/" + getRandomSuite() + "-" + firstCard + ".jpg'> <img src='./playing-cards/" + getRandomSuite() + "-" + secondCard + ".jpg'>"
     console.log(cardReal.innerhtml)
     cards = [getCardValue(firstCard), getCardValue(secondCard)]
     sum = getCardValue(firstCard) + getCardValue(secondCard)
@@ -59,13 +66,13 @@ function renderGame() {
     //     cardsEl.textContent += cards[i] + " "
     // }
 
-    dealerCards.innerHTML = "<img src='/playing-cards/front.jpg' />"
-    stay = false
-
     sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
+    if (sum <= 20 && stay === false) {
         message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
+    } else if (sum > dealerSum && stay && sum < 21 ) {
+        message = "You won!"
+        hasBlackJack = true
+    }else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
@@ -79,7 +86,7 @@ function renderGame() {
 function newCard() {
     if (isAlive === true && hasBlackJack === false && stay === false) {
         let card = getRandomCard()
-        cardReal.innerHTML += " <img src='/playing-cards/" + getRandomSuite() + "-" + card + ".jpg'>"
+        cardReal.innerHTML += " <img src='./playing-cards/" + getRandomSuite() + "-" + card + ".jpg'>"
         sum += getCardValue(card)
         cards.push(getCardValue(card))
 
@@ -92,10 +99,13 @@ function stayButton() {
     if (isAlive === true && hasBlackJack === false && stay === false) {
         let dealerFirstCard = getRandomCard()
         let dealerSecondCard = getRandomCard()
-        console.log("hi")
+        dealerSum = getCardValue(dealerFirstCard) + getCardValue(dealerSecondCard)
+        dealerSumEl.textContent = "Dealer: " + dealerSum
 
-        dealerCards.innerHTML = "<img src='/playing-cards/" + getRandomSuite() + "-" + dealerFirstCard + ".jpg'> <img src='/playing-cards/" + getRandomSuite() + "-" + dealerSecondCard + ".jpg'>"
+        dealerCardsEl.innerHTML = "<img src='./playing-cards/" + getRandomSuite() + "-" + dealerFirstCard + ".jpg'> <img src='./playing-cards/" + getRandomSuite() + "-" + dealerSecondCard + ".jpg'>"
 
         stay = true
+        console.log(stay)
+        renderGame()
     }
 }
